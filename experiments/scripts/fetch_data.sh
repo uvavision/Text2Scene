d6992637d95aeadf9afa75078848c10c533a10ab
FUWEN TAN <fuwen.tan@gmail.com>
2019-10-11 15:07:51 -0400

    also download the stuff annotation

--- experiments/scripts/fetch_data.sh
+++ experiments/scripts/fetch_data.sh
@@ -5,6 +5,7 @@ COCO_TRAIN=http://images.cocodataset.org/zips/train2017.zip
 COCO_VAL=http://images.cocodataset.org/zips/val2017.zip
 COCO_ANNOTATION_17=http://images.cocodataset.org/annotations/annotations_trainval2017.zip
 COCO_ANNOTATION_14=http://images.cocodataset.org/annotations/annotations_trainval2014.zip
+COCO_STUFF_17=http://images.cocodataset.org/annotations/stuff_annotations_trainval2017.zip
 
 echo "Downloading Abstract Scene ..."
 wget $ABSTRACT -O AbstractScenes_v1.1.zip
@@ -24,8 +25,10 @@ unzip -q val2017.zip -d data/coco/images
 echo "Downloading coco annotation data ..."
 wget $COCO_ANNOTATION_17 -O annotations_trainval2017.zip
 wget $COCO_ANNOTATION_14 -O annotations_trainval2014.zip
+wget $COCO_STUFF_17 -O stuff_annotations_trainval2017.zip
 echo "Unzipping..."
 unzip -q annotations_trainval2017.zip -d data/coco
 unzip -q annotations_trainval2014.zip -d data/coco
+unzip -q stuff_annotations_trainval2017.zip -d data/coco
 
-rm AbstractScenes_v1.1.zip train2017.zip val2017.zip annotations_trainval2017.zip annotations_trainval2014.zip 
+rm AbstractScenes_v1.1.zip train2017.zip val2017.zip annotations_trainval2017.zip annotations_trainval2014.zip stuff_annotations_trainval2017.zip
