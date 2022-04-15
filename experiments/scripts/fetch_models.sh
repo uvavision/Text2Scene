2ee5bb6490359459cb3b5812021a32e35e1f8309
fwtan <fuwen.tan@gmail.com>
2022-04-15 13:12:33 +0100

    Update fetch_models.sh

--- experiments/scripts/fetch_models.sh
+++ experiments/scripts/fetch_models.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PRTRAINED_URL=www.cs.virginia.edu/~ft3ex/data/text2scene/pretrained.zip
+PRTRAINED_URL=https://www.cs.rice.edu/~vo9/fuwen/text2scene/pretrained.zip
 
 echo "Downloading pretrained model ..."
 wget $PRTRAINED_URL -O pretrained.zip
